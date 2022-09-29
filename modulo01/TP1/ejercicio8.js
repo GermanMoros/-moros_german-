8445092392e341c93c730a9d51468050239ff647
GermanMoros <111381426+GermanMoros@users.noreply.github.com>
2022-09-29 16:10:56 -0300

    Create ejercicio8.js

---
+++ modulo01/TP1/ejercicio8.js
@@ -0,0 +1,8 @@
+function numeros(min,max) {
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    return Math.floor(Math.random() * (max - min) + min);
+}
+for (i = 0; i < 100; i++){
+console.log (numeros(1,100))
+}
