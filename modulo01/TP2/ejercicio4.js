e0866f7605cdf2f0a2ee6018fb161eac502bb757
GermanMoros <111381426+GermanMoros@users.noreply.github.com>
2022-09-29 16:22:11 -0300

    Create ejercicio4.js

---
+++ modulo01/TP2/ejercicio4.js
@@ -0,0 +1,7 @@
+let numero;
+numero = prompt ("Introduzca un número");
+if (numero % 2 == 0 ){
+    console.log ("El número"+ numero + " es Par")
+} else {
+    console.log ("El número"+ numero + " es Impar")
+}
