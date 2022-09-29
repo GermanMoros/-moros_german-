05d518b89ffd29ac984f529aab31273a73a28f73
GermanMoros <111381426+GermanMoros@users.noreply.github.com>
2022-09-29 16:24:54 -0300

    Create ejercicio7.js

---
+++ modulo01/TP2/ejercicio7.js
@@ -0,0 +1,10 @@
+function invertir (str) {
+    if (str === "") {
+        return "";
+    } else {
+        return invertir(str.substr(1)) + str.charAt(0);
+    }
+}
+
+let invertirString = invertir("Ejercicio Silicon")
+console.log(invertirString)
