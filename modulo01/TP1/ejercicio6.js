0a7f9207ab6565531c886cc3c5a9d831bd9b5a32
GermanMoros <111381426+GermanMoros@users.noreply.github.com>
2022-09-29 16:09:39 -0300

    Create ejercicio6.js

---
+++ modulo01/TP1/ejercicio6.js
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+<head>
+<h1>Dias De la semana</h1>
+</head>
+<body>
+<script>
+var days = ["lunes <br>","martes <br>", "miercoles <br>","Jueves <br>","Viernes <br>","Sabado <br>", "Domingo<br>"];
+for(var i = 1; i<= days.length;i++){
+}
+console.log (days);
+document.writeln (days);
+</script>
+
+</body>
