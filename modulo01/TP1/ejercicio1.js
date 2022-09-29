1555f79c3ad193f07a1aa1fe2b582b8481bf8349
GermanMoros <111381426+GermanMoros@users.noreply.github.com>
2022-09-29 16:06:18 -0300

    Update ejercicio1.js

--- modulo01/TP1/ejercicio1.js
+++ modulo01/TP1/ejercicio1.js
@@ -1,1 +1,13 @@
-
+<!DOCTYPE html>
+<html>
+<head>
+<h1>Hola mundo </h1>
+<h2>Silicon </h2>>
+</head>
+<body>
+<script>
+document.writeln("Soy el primer script")
+document.writeln("")
+</script>
+</body>
+</html>
